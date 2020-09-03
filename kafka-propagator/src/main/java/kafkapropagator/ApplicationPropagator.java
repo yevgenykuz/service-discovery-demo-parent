5dc130f040ea0105a91613b4cedabc53692a646f
ofertavivcx <57224523+ofertavivcx@users.noreply.github.com>
2020-09-03 12:30:42 +0300

    Add kafka applications (#5)

---
+++ kafka-propagator/src/main/java/kafkapropagator/ApplicationPropagator.java
@@ -0,0 +1,11 @@
+package kafkapropagator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationPropagator {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationPropagator.class, args);
+    }
+}
