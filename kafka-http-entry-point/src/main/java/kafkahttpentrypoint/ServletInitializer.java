cf7e0ceabe24525cd2f7e7c0bbab4962c93134aa
ofertavivcx <ofer.taviv@checkmarx.com>
2020-09-17 11:55:11 +0300

    update spring kafka version

--- kafka-http-entry-point/src/main/java/kafkahttpentrypoint/ServletInitializer.java
+++ kafka-http-entry-point/src/main/java/kafkahttpentrypoint/ServletInitializer.java
@@ -1,7 +1,8 @@
 package kafkahttpentrypoint;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
