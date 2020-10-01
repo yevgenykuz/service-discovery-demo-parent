614f58e7d447ed998300141e9e239e693cf02c7d
ofertavivcx <57224523+ofertavivcx@users.noreply.github.com>
2020-10-01 09:35:18 +0300

    Update java-apps/java-rabbitmq-propagator/run.bat
    Co-authored-by: Yevgeny Kuznetsov <yevgenyku@gmail.com>

--- java-apps/java-rabbitmq-propagator/run.bat
+++ java-apps/java-rabbitmq-propagator/run.bat
@@ -1,1 +1,1 @@
-java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=1011 -javaagent:"agent\cx-launcher.jar" -DcxAgentAutoUpgrade=no -Dcx.standalone=yes -Dcx.log.level=DEBUG -jar "target\java-rabbitmq-propagator.jar"
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8419 -javaagent:"agent\cx-launcher.jar" -DcxAgentAutoUpgrade=no -Dcx.standalone=yes -Dcx.log.level=DEBUG -jar "target\java-rabbitmq-propagator.jar"
