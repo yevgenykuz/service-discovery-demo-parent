f7c103a94d1f7bc29bc4c671381f2d1c8f5964bb
ofertavivcx <57224523+ofertavivcx@users.noreply.github.com>
2020-10-01 09:33:51 +0300

    Update java-apps/java-rabbitmq-http-entry-point/src/main/java/rabbitmqhttpentrypoint/controller/HomeController.java
    Co-authored-by: Yevgeny Kuznetsov <yevgenyku@gmail.com>

--- java-apps/java-rabbitmq-http-entry-point/src/main/java/rabbitmqhttpentrypoint/controller/HomeController.java
+++ java-apps/java-rabbitmq-http-entry-point/src/main/java/rabbitmqhttpentrypoint/controller/HomeController.java
@@ -3,9 +3,6 @@ package rabbitmqhttpentrypoint.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Home page hybrid.controller class.
- */
 @RestController
 public class HomeController {
 
