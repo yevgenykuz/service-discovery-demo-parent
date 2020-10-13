28537f4588758130d0ff40b148898e52c25645c2
AsafH <asaf.hilely@checkmarx.com>
2020-10-13 11:23:38 +0300

    add start.sh to rabbit apps and change server_ip to iast_manager_ip

--- java-apps/java-kafka-sink/start.sh
+++ java-apps/java-kafka-sink/start.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
-if [[ -z "${server_ip}" ]]; then
+if [[ -z "${IAST_MANAGER_IP}" ]]; then
   java -jar java-kafka-sink.jar
 else
   cwd=$(pwd)
   mkdir /usr/local/cxiast-agent
   cd /usr/local/cxiast-agent
-  wget "http://${server_ip}:8380/iast/compilation/download" -O CxAgent.zip
+  wget "http://${IAST_MANAGER_IP}:8380/iast/compilation/download" -O CxAgent.zip
   unzip CxAgent.zip
   chmod a+w .
   chmod a+x CxIAST.sh
