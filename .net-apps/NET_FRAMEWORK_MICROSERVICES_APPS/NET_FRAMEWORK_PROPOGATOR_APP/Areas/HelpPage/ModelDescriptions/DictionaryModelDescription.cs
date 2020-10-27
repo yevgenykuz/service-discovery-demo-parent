e22c6f1a7ee37f59847434010a55a9cec700853a
snirshemtov <34773946+snirshemtov@users.noreply.github.com>
2020-10-27 13:45:41 +0200

    removed NET_FRAMEWORK_APPS

--- .net-apps/NET_FRAMEWORK_MICROSERVICES_APPS/NET_FRAMEWORK_PROPOGATOR_APP/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace NET_FRAMEWORK_PROPOGATOR_APP.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- .net-apps/NET_FRAMEWORK_MICROSERVICES_APPS/NET_FRAMEWORK_SINK_APP/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace NET_FRAMEWORK_SINK_APP.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
