d63284839163a47f2f5232edfc126fc61e3a44e7
snirshemtov <34773946+snirshemtov@users.noreply.github.com>
2020-10-28 10:26:54 +0200

    fixed start.sh

--- .net-core-apps/NET_CORE_REST_ENTRY_POINT_APP/start.sh
+++ .net-core-apps/NET_CORE_REST_ENTRY_POINT_APP/start.sh
@@ -12,5 +12,5 @@ else
   chmod a+x CxIAST_CORE.sh
   echo "maxStoredStringLength=50" >>cx_agent.override.properties
   cd ${cwd}
-  /usr/local/cxiast-agent/CxIAST_CORE.sh "dotnet /app/bin/Debug/netcoreapp2.1/NET_CORE_SINK_APP.dll"
+  /usr/local/cxiast-agent/CxIAST_CORE.sh "dotnet /app/bin/Debug/netcoreapp2.1/NET_CORE_REST_ENTRY_POINT_APP.dll"
 fi
