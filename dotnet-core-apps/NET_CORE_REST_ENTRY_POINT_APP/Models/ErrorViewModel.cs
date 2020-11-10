074a1d457a4c73434d30701f07e404a348cd522e
snirshemtov <34773946+snirshemtov@users.noreply.github.com>
2020-11-10 10:13:57 +0200

    Added home page for NET_CORE_REST_ENTRY_POINT_APP

---
+++ dotnet-core-apps/NET_CORE_REST_ENTRY_POINT_APP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Entry.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- dotnet-core-apps/NET_CORE_REST_ENTRY_POINT_APP/Startup.cs
+++ dotnet-core-apps/NET_CORE_REST_ENTRY_POINT_APP/Startup.cs
@@ -41,7 +41,12 @@ namespace CoreMicroService_A
             }
 
             app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
