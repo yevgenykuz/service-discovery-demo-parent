756d3334a66e71b2cd8dbf064a45b167c798e50d
ofertavivcx <ofer.taviv@checkmarx.com>
2020-11-12 14:19:55 +0200

    hackathon

--- kotlin-apps/kotlin-http-middleman/start.sh
+++ kotlin-apps/kotlin-http-middleman/start.sh
@@ -1,2 +1,13 @@
 #! /bin/bash
-java -jar kotlin-http-middleman.jar
+if [[ -z "${IAST_MANAGER_URL}" ]]; then
+  java -jar kotlin-http-middleman.jar
+else
+  cwd=$(pwd)
+  mkdir /usr/local/cxiast-agent
+  cd /usr/local/cxiast-agent
+  wget "${IAST_MANAGER_URL}/iast/compilation/download/LIGHTWEIGHT" -O CxAgent.zip
+  unzip CxAgent.zip
+  python3 ./main.py &
+  cd ${cwd}
+  java -jar kotlin-http-middleman.jar
+fi
