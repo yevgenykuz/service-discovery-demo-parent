b5cf481715efe4904c7a623757a66f7304efb75c
Moris Rafol <moris.rafol@checkmarx.com>
2020-11-30 09:33:19 +0200

    feat: redesign navbar

--- java-apps/java-http-ui/src/components/navBar/navBar.js
+++ java-apps/java-http-ui/src/components/navBar/navBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Link, useHistory} from "react-router-dom";
-import {CHECK_BALANCE, DEPOSIT} from "../../constants/routes";
+import {Link, useHistory,useLocation} from "react-router-dom";
+import {CHECK_BALANCE, DEPOSIT, HOME} from "../../constants/routes";
 import useIsLoggedInState from "../../recoilStates/userAuth";
 import * as Auth from "../../models/auth";
 import * as routes from "../../constants/routes";
@@ -10,6 +10,7 @@ import {Nav, Navbar} from "react-bootstrap";
 function NavBar() {
 
     const history = useHistory()
+    let location = useLocation();
     const [, setIsLoggedIn] = useIsLoggedInState()
     function handleLogout(){
         Auth.logout()
@@ -19,19 +20,32 @@ function NavBar() {
             })
     }
 
+    function getSelectedClassname(route){
+        if(location.pathname === "/" && route === "/")
+            return styles.selected;
+        else if ( route !=="/" && location.pathname.toLowerCase().startsWith(route.toLowerCase()))
+            return styles.selected;
+        return ""
+    }
+
     return (
         <Navbar className={styles.component} bg={"dark"} variant={"dark"}>
-           <Navbar.Brand>Checkmarx Bank</Navbar.Brand>
+            <Navbar.Brand ><span className={styles.logoText}>Checkmarx Bank</span></Navbar.Brand>
+
             <Nav.Link>
-                <Link to={DEPOSIT}>Deposit</Link>
+                <Link to={HOME}><span className={`${styles.buttons} ${getSelectedClassname(HOME)}`}>Home</span></Link>
             </Nav.Link>
 
             <Nav.Link>
-                <Link to={CHECK_BALANCE}>Check Balance</Link>
+                <Link to={DEPOSIT}><span className={`${styles.buttons} ${getSelectedClassname(DEPOSIT)}`}>Deposit</span></Link>
+            </Nav.Link>
+
+            <Nav.Link>
+                <Link to={CHECK_BALANCE}><span className={`${styles.buttons} ${getSelectedClassname(CHECK_BALANCE)}`}>Check Balance</span></Link>
             </Nav.Link>
 
             <Nav.Link  onClick={handleLogout}>
-                Logout
+               <span className={styles.buttons}>Logout</span>
             </Nav.Link>
 
         </Navbar>
