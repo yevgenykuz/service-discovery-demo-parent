1259e0d1a48ab61bb2c2a297833c5a3bd288d124
Moris Rafol <moris.rafol@checkmarx.com>
2020-11-30 12:13:19 +0200

    feat: add background image

--- java-apps/java-http-ui/src/components/components/screenWrapper/screenWrapper.js
+++ java-apps/java-http-ui/src/components/components/screenWrapper/screenWrapper.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import styles from "./screenWrapper.module.css"
 
-function ScreenWrapper({className = "",...props}) {
+function ScreenWrapper({className = "",children,...props}) {
     return (
-        <div className={`${styles.component} ${className}`} {...props}/>
+        <div className={styles.component} {...props}>
+            <div className={styles.background} style={{backgroundImage:`url("/img/bitcoinBackground/image.jpg")`}} />
+            <div className={`${styles.content} ${className}`}>
+            {children}
+            </div>
+
+        </div>
+
     );
 }
 
