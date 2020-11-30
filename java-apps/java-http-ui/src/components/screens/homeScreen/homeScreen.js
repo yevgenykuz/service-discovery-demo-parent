657fb114baaa6420ccf2472ae77503fe295b5c7f
Moris Rafol <moris.rafol@checkmarx.com>
2020-11-30 12:25:20 +0200

    feat: surround home screen content with card

--- java-apps/java-http-ui/src/components/screens/homeScreen/homeScreen.js
+++ java-apps/java-http-ui/src/components/screens/homeScreen/homeScreen.js
@@ -2,13 +2,16 @@ import React from 'react';
 import styles from "./homeScreen.module.css"
 import ScreenWrapper from "../../components/screenWrapper";
 import {useUserInfo} from "../../../recoilStates/userAuth";
+import CardWrapper from "../../components/cardWrapper";
 
 function HomeScreen(props) {
 
     const [userInfo] = useUserInfo()
     return (
-        <ScreenWrapper className={styles.component}>
-            <h3>Welcome back {userInfo?.username}</h3>
+        <ScreenWrapper className={`flexCenter ${styles.component}`}>
+            <CardWrapper className={styles.content}>
+                <h3>Welcome back {userInfo?.username}</h3>
+            </CardWrapper>
 
         </ScreenWrapper>
     );
