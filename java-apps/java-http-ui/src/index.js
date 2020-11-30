ce3f51781ec22a159ae66206d589b552874b2e37
Moris Rafol <moris.rafol@checkmarx.com>
2020-11-30 17:12:01 +0200

    feat: serialize and load logs

--- java-apps/java-http-ui/src/index.js
+++ java-apps/java-http-ui/src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom"
 import {RecoilRoot} from "recoil";
+import LogsLocalStorageManager from "./components/logic/logsLocalStorageManager";
 
 ReactDOM.render(
     <React.StrictMode>
         <RecoilRoot>
             <BrowserRouter>
+                <LogsLocalStorageManager/>
                 <App/>
             </BrowserRouter>
         </RecoilRoot>
