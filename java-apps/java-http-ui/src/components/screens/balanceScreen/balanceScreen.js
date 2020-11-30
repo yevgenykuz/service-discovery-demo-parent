46bf1d42ca2d9424716ef68271143e051cd0ca42
Moris Rafol <moris.rafol@checkmarx.com>
2020-11-30 10:52:34 +0200

    feat: redesign the balance screen

--- java-apps/java-http-ui/src/components/screens/balanceScreen/balanceScreen.js
+++ java-apps/java-http-ui/src/components/screens/balanceScreen/balanceScreen.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ScreenWrapper from "../../components/screenWrapper";
 import {checkBalance} from "../../../models/API";
-
+import LoadingPopup from "../../components/loadingPopup";
+import CardWrapper from "../../components/cardWrapper";
+import styles from "./balanceScreen.module.css"
 function BalanceScreen() {
 
     const [balance, setBalance] = React.useState()
@@ -14,11 +16,18 @@ function BalanceScreen() {
         })
     }, [])
 
-        return (
-            <ScreenWrapper>
-                {isLoading? "loading..." : balance}
-            </ScreenWrapper>
-        );
+    return (
+        <ScreenWrapper className={`flexCenter`}>
+            {isLoading ? <LoadingPopup/> :
+                <CardWrapper className={styles.content}>
+                    <h6>You're current balance is:</h6>
+                    <p>
+                    {/*add up/down arrow icon */}
+                    <h3 className={balance>=0?styles.money_plus:styles.money_minus}>{balance}$</h3>
+                    </p>
+                </CardWrapper>}
+        </ScreenWrapper>
+    );
 
 
 }
