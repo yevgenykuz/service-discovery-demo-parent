c859e62d6e4f6f9a9a53688e9a5ba75887d0d2f0
Moris Rafol <moris.rafol@checkmarx.com>
2020-11-30 11:19:39 +0200

    feat: redesign the deposit screen

--- java-apps/java-http-ui/src/components/screens/depositScreen/depositScreen.js
+++ java-apps/java-http-ui/src/components/screens/depositScreen/depositScreen.js
@@ -1,25 +1,48 @@
 import React from 'react';
 import {depositAmount} from "../../../models/API";
 import ScreenWrapper from "../../components/screenWrapper";
+import styles from "./deposit.module.css"
+import LoadingPopup from "../../components/loadingPopup";
+import CardWrapper from "../../components/cardWrapper";
+import {Alert, Button, Form, Toast} from "react-bootstrap";
+
+const pageStates = {
+    loading: "loading",
+    beforeDeposit: "beforeDeposit",
+    afterDeposit: "afterDeposit"
+}
 
 function DepositScreen(props) {
 
-    const [amount,setAmount] = React.useState(1)
-    const [isLoading,setIsLoading] = React.useState(false)
+    const [amount, setAmount] = React.useState(1)
+    const [pageState, setPageState] = React.useState(pageStates.beforeDeposit)
 
-    function handleSubmit(e){
+    function handleSubmit(e) {
         e.preventDefault()
-        setIsLoading(true)
-        depositAmount(amount).then(res=>setIsLoading(false))
+        setPageState(pageStates.loading)
+        depositAmount(amount).then(res => setPageState(pageStates.afterDeposit))
     }
 
     return (
-        <ScreenWrapper>
-        <form onSubmit={handleSubmit}>
-            <input type="number" min={1} value={amount} onChange={e=>setAmount(Number(e.target.value))}/>
-            <input type="submit"/>
-            {isLoading&&"loading..."}
-        </form>
+        <ScreenWrapper className={`flexCenter flexColumn ${styles.component}`}>
+            {pageState === pageStates.loading ? <LoadingPopup/> :
+                    <div className={styles.contentContainer}>
+                    <CardWrapper className={styles.contentCard}>
+                        <Form className={styles.form} onSubmit={handleSubmit}>
+                            <Form.Group>
+                                <Form.Label>Enter the amount to deposit</Form.Label>
+                                <Form.Control type="number" min={1} value={amount}
+                                              onChange={e => setAmount(Number(e.target.value))}/>
+
+                            </Form.Group>
+                            <Button variant={"info"} className={styles.depositButton} type="submit">Deposit</Button>
+
+                        </Form>
+                    </CardWrapper>
+                    {pageState === pageStates.afterDeposit && <Alert className={styles.popup} variant={"success"}>The deposit has been successfully transmitted</Alert>}
+                    </div>
+
+               }
         </ScreenWrapper>
 
     );
