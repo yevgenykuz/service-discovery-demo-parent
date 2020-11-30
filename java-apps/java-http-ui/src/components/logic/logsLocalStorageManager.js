02da6a94395ce7cb52410660fc2e68947b6d47ca
Moris Rafol <moris.rafol@checkmarx.com>
2020-11-30 17:12:01 +0200

    feat: serialize and load logs

---
+++ java-apps/java-http-ui/src/components/logic/logsLocalStorageManager.js
@@ -0,0 +1,21 @@
+import React, {useEffect, useState} from 'react';
+import useLogger from "../../recoilStates/logger";
+
+function LogsLocalStorageManager(props) {
+    const logger = useLogger()
+    const [loaded, setLoaded] = useState(false)
+
+    useEffect(() => {
+        if (!loaded) {
+            const logs = JSON.parse(localStorage.getItem("logs") || "[]")
+            logger.setLogs(logs)
+            setLoaded(true)
+        }
+        localStorage.setItem("logs", JSON.stringify(logger.logs))
+        console.log(logger.logs)
+    }, [logger.logs])
+
+    return <></>
+}
+
+export default LogsLocalStorageManager;
