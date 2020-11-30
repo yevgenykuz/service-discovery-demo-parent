e2bb7f0f20624221ba10720345f21e197a479f02
Moris Rafol <moris.rafol@checkmarx.com>
2020-11-30 14:57:04 +0200

    feat: changes

--- java-apps/java-http-ui/src/components/components/homeMenuButton/homeMenuButton.js
+++ java-apps/java-http-ui/src/components/components/homeMenuButton/homeMenuButton.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import styles from "./homeMenuButton.module.css"
 
-function HomeMenuButton({size = "200px", iconSrc = "", label = "", onClick, className = "", style, ...props}) {
+function HomeMenuButton({size = "150px", color = "white", textColor = "black", iconSrc = "", label = "", onClick, className = "", style, ...props}) {
 
-    return (<div style={{...style, width: size.toString(), height: size.toString()}}
+    return (<div style={{
+            ...style,
+            width: size.toString(),
+            height: size.toString(),
+            backgroundColor: color,
+            color: textColor
+        }}
                  className={`${styles.component} ${className}`} {...props} onClick={onClick}>
-            {iconSrc && <img src={iconSrc} alt={label}/>}
-            <span>{label}</span>
+            {iconSrc && <img src={iconSrc} alt={label} style={{
+                ...style,
+                width: `calc( ${size.toString()} / 3 )`,
+                height: `calc( ${size.toString()} / 3 )`
+            }}/>}
+            {label && <span>{label}</span>}
         </div>
     );
 }
