66cbd9cc988a8909be98b3f14e6dfdef5cb16873
Moris Rafol <moris.rafol@checkmarx.com>
2020-11-30 14:52:14 +0200

    feat: add home menu button props

--- java-apps/java-http-ui/src/components/screens/homeScreen/homeScreen.js
+++ java-apps/java-http-ui/src/components/screens/homeScreen/homeScreen.js
@@ -3,15 +3,22 @@ import styles from "./homeScreen.module.css"
 import ScreenWrapper from "../../components/screenWrapper";
 import {useUserInfo} from "../../../recoilStates/userAuth";
 import CardWrapper from "../../components/cardWrapper";
+import HomeMenuButton from "../../components/homeMenuButton";
 
-function HomeScreen(props) {
+function HomeScreen() {
 
     const [userInfo] = useUserInfo()
     return (
-        <ScreenWrapper className={`flexCenter ${styles.component}`}>
-            <CardWrapper className={styles.content}>
+        <ScreenWrapper className={`flexCenter flexColumn ${styles.component}`}>
+
+            <CardWrapper className={styles.welcome}>
                 <h3>Welcome back {userInfo?.username}</h3>
             </CardWrapper>
+
+        <div className={styles.menu}>
+            <HomeMenuButton label={"deposit"} iconSrc={"/img/deposit.svg"} />
+            <HomeMenuButton label={"check balance"}/>
+        </div>
 
         </ScreenWrapper>
     );
