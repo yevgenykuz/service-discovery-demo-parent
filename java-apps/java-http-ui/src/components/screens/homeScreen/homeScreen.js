16c10f4fec5eb68c3f305817ee8b0f1fbc8b5e18
Moris Rafol <moris.rafol@checkmarx.com>
2020-11-30 09:10:23 +0200

    feat: add navbar

--- java-apps/java-http-ui/src/components/screens/homeScreen/homeScreen.js
+++ java-apps/java-http-ui/src/components/screens/homeScreen/homeScreen.js
@@ -1,33 +1,13 @@
 import React from 'react';
-import {Link, useHistory} from "react-router-dom";
-import {CHECK_BALANCE, DEPOSIT} from "../../../constants/routes";
-import * as Auth from "../../../models/auth";
-import * as routes from "../../../constants/routes";
-import useIsLoggedInState from "../../../recoilStates/userAuth";
 import styles from "./homeScreen.module.css"
 import ScreenWrapper from "../../components/screenWrapper";
 
 function HomeScreen(props) {
 
-    const history = useHistory()
-    const [, setIsLoggedIn] = useIsLoggedInState()
-    function handleLogout(){
-        Auth.logout()
-            .then(()=>{
-                setIsLoggedIn(Auth.isLoggedIn())
-                history.push(routes.LOGIN)
-            })
-    }
 
     return (
         <ScreenWrapper className={styles.component}>
-            <Link to={DEPOSIT}>
-                <button>deposit</button>
-            </Link>
-            <Link to={CHECK_BALANCE}>
-                <button>check balance</button>
-            </Link>
-            <button onClick={handleLogout}>logout</button>
+
 
         </ScreenWrapper>
     );
