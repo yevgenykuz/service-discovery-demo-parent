f96e244e55c8a8b57235c686b74e109cec8de125
Moris Rafol <moris.rafol@checkmarx.com>
2020-11-30 10:24:03 +0200

    refactor: add timeout to depositScreen

--- java-apps/java-http-ui/src/models/API.js
+++ java-apps/java-http-ui/src/models/API.js
@@ -1,14 +1,20 @@
 import axios from "axios"
 
-const DEPOSIT_ROUTE = "localhost:8110/prop-name"
-const CHECK_BALANCE_ROUTE = "localhost:8110/name"
+const DEPOSIT_ROUTE = "http://localhost:8110/prop-name"
+const CHECK_BALANCE_ROUTE = "http://localhost:8110/name"
 
+function timeoutPromise(time = 1000){
+    return new Promise((resolve) => setTimeout(resolve,time))
+}
 
-export function depositAmount(amount) {
-    return axios.get(`${DEPOSIT_ROUTE}?name=${amount}`)
+export async function depositAmount(amount) {
+    await timeoutPromise(2000)
+    // await axios.get(`${DEPOSIT_ROUTE}?name=${amount}`)
+
 }
 
 export async function checkBalance() {
-    return new Promise((resolve) => setTimeout(()=>resolve(200),3000))
-    // return await axios.get(CHECK_BALANCE_ROUTE)
+     await timeoutPromise(2000)
+     // await axios.get(`${CHECK_BALANCE_ROUTE}?name=test`)
+    return 100
 }
