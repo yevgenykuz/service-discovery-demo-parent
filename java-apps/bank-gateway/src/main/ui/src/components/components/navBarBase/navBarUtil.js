d7dc1a02a17231a9ff9fa607ce157066de6e2743
Moris Rafol <moris.rafol@checkmarx.com>
2020-12-15 01:10:24 +0200

    fix: nav bar entry not highlighting

--- java-apps/bank-gateway/src/main/ui/src/components/components/navBarBase/navBarUtil.js
+++ java-apps/bank-gateway/src/main/ui/src/components/components/navBarBase/navBarUtil.js
@@ -1,9 +1,11 @@
 import styles from "./navBarBase.module.css";
 
-export function getSelectedClassname(pathname,route){
-    if(pathname === "/" && route === "/")
+export function getSelectedClassname(pathname, route) {
+    if (pathname === "/" && route === "/")
         return styles.buttonSelected;
-    else if ( route !=="/" && pathname.toLowerCase().startsWith(route.toLowerCase()))
-        return styles.buttonSelected;
+    else if (route !== "/" && pathname.toLowerCase().startsWith(route.toLowerCase()))
+    {
+        return styles["button--selected"];
+    }
     return ""
 }
