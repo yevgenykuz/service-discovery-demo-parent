63c052d7e4a4c714d648fb0c1b9a7a6b2875a819
Moris Rafol <moris.rafol@checkmarx.com>
2020-12-14 17:52:20 +0200

    feat: finish designing the convertCurrencyScreen (w/o logic)

--- java-apps/bank-gateway/src/main/ui/src/components/screens/convertCurrencyScreen/convertCurrencyScreen.js
+++ java-apps/bank-gateway/src/main/ui/src/components/screens/convertCurrencyScreen/convertCurrencyScreen.js
@@ -1,41 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ScreenWrapper from "../../components/screenWrapper";
 import CardWrapper from "../../components/cardWrapper";
-import {Dropdown, Form} from "react-bootstrap";
+import {Button, Form} from "react-bootstrap";
 import {getAllOptions} from "../../../constants/convertCurrencyOptions";
+import styles from "./convertCurrencyScreen.module.css"
 
+function ConvertCurrencyScreen() {
 
-function ConvertCurrencyScreen(props) {
+    const [fromCur, setFromCur] = useState("")
+    const [toCur, setToCur] = useState("")
+    const [amount, setAmount] = useState(0)
+    const toCurOptions = getAllOptions().filter(cur=>cur!== fromCur)
     return (<ScreenWrapper className="flexCenter">
-            <CardWrapper >
+            <CardWrapper className={styles.card}>
                 <img src={"/img/exchange.svg"} alt={"deposit icon"} className={`iconMarginMedium iconMedium`}/>
-                <Form>
+                <Form className={styles.form}>
+
                     <Form.Group>
                         <Form.Label>Enter the amount to convert</Form.Label>
-                        <Form.Control type='number' />
+                        <Form.Control required type='number' value={amount} onChange={(e) =>
+                            setAmount(e.target.value ? Number(e.target.value) : "")}
+                        />
                     </Form.Group>
-                    <Form.Group>
-                        <Form.Label>From</Form.Label>
-                        <Dropdown>
-                            <Dropdown.Toggle variant="success" id="dropdown-basic">
-                                From
-                            </Dropdown.Toggle>
-                            <Dropdown.Menu>
-                                {getAllOptions().map((currencyType)=> <Dropdown.Item key={`from_${currencyType}`} >{currencyType}</Dropdown.Item>)}
-                            </Dropdown.Menu>
-                        </Dropdown>
-                    </Form.Group>
-                    <Form.Group>
-                        <Dropdown>
-                            <Dropdown.Toggle variant="success" id="dropdown-basic">
-                                To
-                            </Dropdown.Toggle>
-                            <Dropdown.Menu>
-                                {getAllOptions().map((currencyType)=> <Dropdown.Item key={`from_${currencyType}`} >{currencyType}</Dropdown.Item>)}
-                            </Dropdown.Menu>
-                        </Dropdown>
 
+                    <Form.Group className={styles.fieldWithLabel}>
+                        <Form.Label>From</Form.Label>
+
+                        <Form.Control as={'select'} value={fromCur||""} required onChange={e => setFromCur(e.target.value)}>
+                            <option value="">Select Currency</option>
+                            {getAllOptions().map((currencyType) =>
+                                <option value={currencyType} key={`from_${currencyType}`}>{currencyType}</option>)}
+                        </Form.Control>
                     </Form.Group>
+
+                    <Form.Group className={styles.fieldWithLabel}>
+                        <Form.Label>To</Form.Label>
+                        <Form.Control as={'select'} value={toCur||""} required onChange={e => setToCur(e.target.value)}>
+                            {toCurOptions.length-1 && <option value="">Select Currency</option>}
+                            {toCurOptions.map((currencyType) =>
+                                <option value={currencyType} key={`to_${currencyType}`}>{currencyType}</option>)
+                              }
+                        </Form.Control>
+                    </Form.Group>
+                    <Button type="submit" className={styles.submitButton} variant={"info"}>Convert</Button>
                 </Form>
 
             </CardWrapper>
