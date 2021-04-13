3bd2768678eade5e74d6fb27d4c150e33b4af399
Gustavo Cortarelli <Gustavo.Cortarelli@checkmarx.com>
2021-04-13 16:28:06 +0100

    added jjwt-jackson dependency on pom file, the jwt.secret property was changed to attend the required minimum size, fixed the @Value annotation on JwtProvider

--- java-apps/bank-gateway/src/main/java/com/checkmarx/demo/service/discovery/http/ep/auth/JwtProvider.java
+++ java-apps/bank-gateway/src/main/java/com/checkmarx/demo/service/discovery/http/ep/auth/JwtProvider.java
@@ -13,7 +13,7 @@ import java.util.Date;
 @Log
 public class JwtProvider {
 
-    @Value("$(jwt.secret)")
+    @Value("${jwt.secret}")
     private String jwtSecret;
 
     public String generateToken(String login) {
